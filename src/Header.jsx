d1bc9884c6cff021965511552df5a1c00b54566a
MandarBorhade <mandarva.borade@gmail.com>
2022-04-10 03:25:26 +0530

    2 files modified

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,14 +4,12 @@ const Header = () => {
 
 
   return(
-    <div className='header'>
-      <div className='wrapper'>
-        <div className='img-container'>
-          <img src="https://img.icons8.com/officel/80/000000/crowd.png"/>
-        </div>
-        <h1>Billsplitz</h1>
-        <h3>Bill splitting simplified..</h3>
+    <div className='wrapper'>
+      <div className='img-container'>
+        <img src="https://img.icons8.com/officel/80/000000/crowd.png"/>
       </div>
+      <h1>Billsplitz</h1>
+      <h3>Bill splitting simplified..</h3>
     </div>
   )
 }
